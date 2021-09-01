0dc1f452d499d5f81899d5ddd256236276215481
内田正輝 <masaki.u@arsaga.jp>
2021-09-01 19:17:07 +0900

    StateFlowRepositoryにUiStateの継承を強制させた

--- src/main/java/jp/co/arsaga/extensions/repositoryFlow/StateFlowRepository.kt
+++ src/main/java/jp/co/arsaga/extensions/repositoryFlow/StateFlowRepository.kt
@@ -2,63 +2,59 @@ package jp.co.arsaga.extensions.repositoryFlow
 
 import jp.co.arsaga.extensions.repository.common.BasePagingRepository
 import jp.co.arsaga.extensions.repository.common.BaseRepository
+import jp.co.arsaga.extensions.repository.common.UiState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
 
 abstract class StateFlowRepository<Res, Req>(
     private val coroutineScope: CoroutineScope,
     initRequest: Req? = null
-) : BaseRepository.Impl<Res?, StateFlow<Res?>, Req>(initRequest) {
+) : BaseRepository.Impl<UiState<Res>, StateFlow<UiState<Res>>, Req>(initRequest) {
 
-    private val _dataSource = DataSource<Res>(coroutineScope, ::onActive, ::onInactive)
+    private val _dataSource = DataSource<Res>(coroutineScope, ::onActive)
 
-    override val dataSource: StateFlow<Res?> = _dataSource.stateFlow
+    override val dataSource: StateFlow<UiState<Res>> = _dataSource.stateFlow
 
-    override fun dataPush(response: Res?) {
+    override fun dataPush(response: UiState<Res>?) {
+        response ?: return
         _dataSource.stateFlow.value = response
     }
 
-    open fun onActive() {
-        if (isNeedUpdate()) refresh()
+    open fun onActive(isActive: Boolean) {
+        if (isActive && isNeedUpdate()) refresh()
     }
-
-    open fun onInactive() {}
 }
 
 abstract class StateFlowPagingRepository<Res, Req, Content>(
     private val coroutineScope: CoroutineScope,
     initRequest: Req? = null
-) : BasePagingRepository.Impl<Res?, StateFlow<Res?>, Req, Content>(initRequest) {
+) : BasePagingRepository.Impl<UiState<Res>, StateFlow<UiState<Res>>, Req, Content>(initRequest) {
 
-    private val _dataSource = DataSource<Res>(coroutineScope, ::onActive, ::onInactive)
+    private val _dataSource = DataSource<Res>(coroutineScope, ::onActive)
 
-    override val dataSource: StateFlow<Res?> = _dataSource.stateFlow
+    override val dataSource: StateFlow<UiState<Res>> = _dataSource.stateFlow
 
     override fun limitEntityCount(): Int = 100
 
-    override fun dataPush(response: Res?) {
+    override fun dataPush(response: UiState<Res>?) {
+        response ?: return
         _dataSource.stateFlow.value = combineList(currentList(), response)
     }
 
-    open fun onActive() {
-        if (isNeedUpdate()) refresh()
+    open fun onActive(isActive: Boolean) {
+        if (isActive && isNeedUpdate()) refresh()
     }
-
-    open fun onInactive() {}
 }
 
 private class DataSource<Res>(
     coroutineScope: CoroutineScope,
-    onActive: () -> Unit,
-    onInactive: () -> Unit
+    onActive: (Boolean) -> Unit
 ) {
-    val stateFlow = MutableStateFlow<Res?>(null).apply {
+    val stateFlow = MutableStateFlow(UiState<Res>()).apply {
         subscriptionCount
             .map { count -> count > 0 }
             .distinctUntilChanged()
-            .onEach {
-                if (it) onActive()
-                else onInactive()
-            }.launchIn(coroutineScope)
+            .onEach { onActive(it) }
+            .launchIn(coroutineScope)
     }
 }
